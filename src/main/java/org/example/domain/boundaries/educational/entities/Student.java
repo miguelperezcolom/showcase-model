e2b2935aed80b1801b976fa811f6b86ba11a0ffa
Miguel Pérez Colom <miguelperezcolom@gmail.com>
2020-10-09 15:47:37 +0200

    added testers for @Css and @EnabledIf

--- src/main/java/org/example/domain/boundaries/educational/entities/Student.java
+++ src/main/java/org/example/domain/boundaries/educational/entities/Student.java
@@ -1,10 +1,11 @@
 package org.example.domain.boundaries.educational.entities;
 
-import io.mateu.mdd.shared.annotations.VisibleIf;
+import io.mateu.mdd.shared.annotations.*;
 import lombok.MateuMDDEntity;
 import org.example.domain.boundaries.common.entities.Person;
 
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
 
 @MateuMDDEntity
 public class Student extends Person {
@@ -16,5 +17,37 @@ public class Student extends Person {
 
     @VisibleIf("$this.scholarshipped")
     String scholarship;
+
+    @NotNull
+    GradeValue averageGrade = GradeValue.N;
+
+    public void setAverageGrade(GradeValue averageGrade) {
+        this.averageGrade = averageGrade;
+
+        if (GradeValue.A.equals(averageGrade) || GradeValue.B.equals(averageGrade)) {
+            classification = "Excellent!";
+            css = "mdd-green-bgd";
+        } else if (GradeValue.C.equals(averageGrade)) {
+            classification = "Not so good";
+            css = "mdd-orange-bgd";
+        } else {
+            classification = "Could improve";
+            css = "mdd-red-bgd";
+        }
+
+
+
+    }
+
+    @Ignored
+    transient String css;
+
+    @Css("$this.css")
+    transient String classification = "Unknown";
+
+
+    @EnabledIf("$this.classification == 'Could improve'")
+    @TextArea
+    String commentOnClassification;
 
 }
