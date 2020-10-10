70ed631aa81058ac18f0cae767779a32e2a276f4
Miguel Pérez Colom <miguelperezcolom@gmail.com>
2020-10-10 08:40:16 +0200

    added fields to test @Columns, @EditableFields and @FilterFields

--- src/main/java/org/example/domain/boundaries/financial/entities/Invoice.java
+++ src/main/java/org/example/domain/boundaries/financial/entities/Invoice.java
@@ -7,6 +7,7 @@ import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
 import java.util.List;
 
 @MateuMDDEntity
@@ -16,10 +17,16 @@ public class Invoice {
     @ManyToOne
     FinancialAgent recipient;
 
+    LocalDate date;
+
+    LocalDate dueDate;
+
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
     List<InvoiceLine> lines;
 
     double total;
+
+    boolean paid;
 
     @ManyToMany(mappedBy = "invoices")
     List<InvoiceTag> tags;
